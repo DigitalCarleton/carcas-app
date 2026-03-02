fe88db734ab01667ed56f72467b29fad0205ac0f
Austin Mason <austin.pj.mason@gmail.com>
2026-03-02 14:18:28 -0600

    Updating comment in spreadapi

--- spreadapi.js
+++ spreadapi.js
@@ -1,9 +1,7 @@
-/*  These are the constants to use for migrating from Sheetdb to Spreadapi
-    The spreadapi documentation is here https://spreadapi.roombelt.com/ 
-    I have done the Setup steps here (https://spreadapi.roombelt.com/setup) with the following settings:
+/*  This document sets the constants for use with Spreadapi (https://spreadapi.roombelt.com/ 
+    The appscript allows public read-only access to the "data" sheet of the Carcas Website Database google sheet:
         User("anonymous", UNSAFE(""), { data: GET });
-    This should allow public read only access to the "data" sheet of the Carcas Website Database google sheet
-    Follow the documentation for Usage and Examples (https://spreadapi.roombelt.com/usage) to replace the sheetdb api with this new one. 
+    See documentation on Usage and Examples here (https://spreadapi.roombelt.com/usage). 
 */
 
 const url = "https://script.google.com/macros/s/AKfycbwUnbEXCjw4HeuTrlK3c108WDyaTPYywD6mD0P9Xut_wPXw4V-CIyC5VDgqOtw7r4vLaA/exec";
